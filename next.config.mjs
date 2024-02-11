cd7c889ec8367ca8c864abea06075ea208d8a23b
ManojVT <reachmanojvt@gmail.com>
2024-02-11 20:09:55 +0530

    Changed config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
-export default nextConfig;
+    /* config options here */
+    output: "export",
+  }
+   
+  export default nextConfig
